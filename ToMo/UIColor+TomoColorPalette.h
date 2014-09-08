7acbbfc57682156e49581d149a8bd1b89889957c
Joe Newbry <joenewbry@gmail.com>
2014-09-08 13:13:02 -0700

    category add on for UIColor to follow ToMo color palette

---
+++ ToMo/UIColor+TomoColorPalette.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+TomoColorPalette.h
+//  ToMo
+//
+//  Created by Joe Newbry on 9/8/14.
+//  Copyright (c) 2014 Joe Newbry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (TomoColorPalette)
+
++ (UIColor *)neonBlueColor;
+
+@end
---
+++ ToMo/UIColor+TomoColorPalette.m
@@ -0,0 +1,18 @@
+//
+//  UIColor+TomoColorPalette.m
+//  ToMo
+//
+//  Created by Joe Newbry on 9/8/14.
+//  Copyright (c) 2014 Joe Newbry. All rights reserved.
+//
+
+#import "UIColor+TomoColorPalette.h"
+
+@implementation UIColor (TomoColorPalette)
+
++ (UIColor *)neonBlueColor
+{
+    return [UIColor colorWithRed:0.0f green:183.0f/245.0f blue:244.0f/245.0f alpha:1.0f];
+}
+
+@end
